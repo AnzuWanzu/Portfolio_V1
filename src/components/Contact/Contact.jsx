d74a8026086493dbac3707e437cedce357d4e202
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-20 16:40:09 +0800

    fixed projects animation issue, changed github link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
-import './contact.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "./contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
 import { CiMail } from "react-icons/ci";
 
-
 const Contact = () => {
-    return (
-        <section id="contact">
-            <h2>Contact Me</h2>
-            <h3><CiMail style={{ 
-                fontSize: '40px', 
-                verticalAlign: 'middle', 
-                marginRight: '5px', 
-                color: 'white' }} /> m.angelinesamson@gmail.com</h3>
-            <div className="contact-info">
-                <a href="https://www.linkedin.com/in/mary-angeline-samson-15728b210/" target="_blank" rel="noopener noreferrer" className="icon-link">
-                    <FontAwesomeIcon icon={faLinkedin} className="icon" />
-                </a>
-                <a href="https://github.com/AngieS-git" target="_blank" rel="noopener noreferrer" className="icon-link">
-                    <FontAwesomeIcon icon={faGithub} className="icon" />
-                </a>
-                <a href="mailto:your.email@example.com" target="_blank" rel="noopener noreferrer" className="icon-link">
-                    <FontAwesomeIcon icon={faGoogle} className="icon" />
-                </a>
-            </div>
-            <p> © 2024 Mary Angeline Samson</p>
-        </section>
-    );
-}
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <h3>
+        <CiMail
+          style={{
+            fontSize: "40px",
+            verticalAlign: "middle",
+            marginRight: "5px",
+            color: "white",
+          }}
+        />{" "}
+        m.angelinesamson@gmail.com
+      </h3>
+      <div className="contact-info">
+        <a
+          href="https://www.linkedin.com/in/mary-angeline-samson-15728b210/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="icon" />
+        </a>
+        <a
+          href="https://github.com/AnzuWanzu"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+        >
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a
+          href="mailto:your.email@example.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+        >
+          <FontAwesomeIcon icon={faGoogle} className="icon" />
+        </a>
+      </div>
+      <p> © 2024 Mary Angeline Samson</p>
+    </section>
+  );
+};
 
 export default Contact;
